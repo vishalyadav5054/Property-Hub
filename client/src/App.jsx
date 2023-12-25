4bca6b07ceed543874febba46fd5d703a33bc905
NextGenVishal <vishal.20215054@mnnit.ac.in>
2023-12-25 15:45:28 +0530

    Created Header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,9 +4,11 @@ import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Home from "./pages/Home";
+import Header from "./components/Header";
 
 export default function App() {
   return <BrowserRouter>
+    <Header />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
